6fb430cc4e0ab9de63e6f5778e3020267291e264
jediminer543 <jediminer543@users.noreply.github.com>
2016-10-19 07:53:06 +0100

    Work on stats

--- src/main/java/org/jmt/starfort/world/entity/stats/StatSheet.java
+++ src/main/java/org/jmt/starfort/world/entity/stats/StatSheet.java
@@ -10,12 +10,30 @@ public class StatSheet {
 	boolean sorted = false;
 	
 	public Stat<?> getStat(String statName) {
+		return getStat(new Stat<Object>(statName, null));
+	}
+	
+	public Stat<?> getStat(Stat<?> stat) {
 		if (!sorted) {
 			Collections.sort(stats);
 			sorted = true;
 		}
-		return stats.get(Collections.binarySearch(stats, new Stat<Object>(statName, null)));
+		Stat<?> out = stats.get(Collections.binarySearch(stats, stat));
+		if (out.getStatName() == stat.getStatName())
+			return out;
+		else
+			return null;
 	}
 	
+	public void addStat(Stat<?> stat) {
+		Stat<?> toMerge = null;
+		if ((toMerge = getStat(stat)) != null) {
+			toMerge
+		}
+	}
+	
+	public static StatSheet merge(StatSheet ss1, StatSheet ss2) {
+		
+	}
 
 }
