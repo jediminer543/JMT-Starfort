3c1b0937eb96e2a0a3e00d0cd440ea621daa9add
jediminer543 <jediminer543@users.noreply.github.com>
2017-01-11 16:10:30 +0000

    Added Travis CI Config and Coveralls config

---
+++ src/main/java/org/jmt/starfort/game/components/debug/ComponentFlag.java
@@ -0,0 +1,32 @@
+package org.jmt.starfort.game.components.debug;
+
+import org.jmt.starfort.world.component.IComponent;
+import org.jmt.starfort.world.component.IComponentTasked;
+import org.jmt.starfort.world.entity.ai.ITask;
+import org.jmt.starfort.world.material.IMaterial;
+
+public class ComponentFlag implements IComponent, IComponentTasked {
+
+	public ComponentFlag() {
+		
+	}
+	
+	@Override
+	public ITask[] getComponentTasks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getComponentName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IMaterial getComponentMaterial() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
