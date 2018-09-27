c8356262e488ea92185e677b0c93261ead46205e
jediminer543 <jediminer543@users.noreply.github.com>
2018-09-27 21:47:36 +0100

    Disabled broken test class

--- src/test/java/org/jmt/starfort/test/processor/ProcessorTest.java
+++ src/test/java/org/jmt/starfort/test/processor/ProcessorTest.java
@@ -8,25 +8,21 @@ import org.jmt.starfort.processor.Processor;
 import org.jmt.starfort.processor.requests.MassProccessRequest;
 import org.jmt.starfort.processor.requests.ProcessingRequest;
 import org.junit.After;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * This test case is borkened, so it is currently disabled
+ * 
+ * @author jediminer543
+ *
+ */
 public class ProcessorTest {
-	
-	@Before
-	public void setUp() throws Exception {
-		Processor.init();
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		
-	}
 	
 	int var = 0;
 	int complete = 0;
 	
-	@Test
+	//@Test
 	public void simpleProcessingTest() {
 		var = 0;
 		complete = 0;
@@ -38,14 +34,16 @@ public class ProcessorTest {
 				complete = 1;
 			}
 		};
+		Processor.init();
 		Processor.addRequest(r);
 		while (complete < 1) {
 			
 		}
 		assertEquals("Processor Runnable Test", var, 10);
+		Processor.down();
 	}
 	
-	@Test
+	//@Test
 	public void massProcessingTest() {
 		var = 0;
 		complete = 0;
@@ -71,12 +69,14 @@ public class ProcessorTest {
 				complete++;
 			}
 		});
+		Processor.init();
 		MassProccessRequest r = new MassProccessRequest(runnables);
 		Processor.addRequest((ProcessingRequest) r);
 		while (complete < 3) {
 			
 		}
 		assertEquals("Processor Runnable Test", 90, var);
+		Processor.down();
 	}
 
 	
